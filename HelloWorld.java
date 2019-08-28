5222c4b7680cf3fd0962d541d6fcac6912552f61
Pavi Bhatter <pbhatter@andrew.cmu.edu>
2019-08-28 13:11:13 -0400

    modified HelloWorld.java program by adding years in school

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("I have been at Carnegie Mellon University for 3 years.");
+    }
+}
